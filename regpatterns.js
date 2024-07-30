3c05d66acbb6096ad42162532d0d184688ae743b
Marek Mráz <mrazbb@gmail.com>
2024-07-30 13:05:02 +0200

    fix bug ( not included default values)

--- regpatterns.js
+++ regpatterns.js
@@ -5,7 +5,7 @@ const end_constraint = /^\s*(CONSTRAINT\s*"(?<name>\w*)"*.+?),?$/gmi;
 const index_line = /CREATE\s+(?:UNIQUE)?\s*INDEX.*$/gmi
 
 const types = ["\\[", "\\]", "bigint", "bigserial", "bit", "bit varying", "boolean", "box", "bytea", "character", "character varying", "cidr", "circle", "date", "double precision", "inet", "integer", "interval", "json", "jsonb", "line", "lseg", "macaddr", "macaddr8", "money", "numeric", "path", "pg_lsn", "pg_snapshot", "point", "polygon", "real", "smallint", "smallserial", "serial", "text", "time", "time with time zone", "timestamp", "timestamp with time zone", "tsquery", "tsvector", "txid_snapshot", "uuid", "xml", "int8", "serial8", "varbit", "bool", "char", "varchar", "float8", "int", "decimal", "int4", "float4", "int2", "serial2", "serial4", "timetz", "timestamptz" ];
-const constraints = [ "NOT NULL", "DEFAULT\\s[\\w\\(\\)]+", "UNIQUE", "PRIMARY KEY\\s*\\(.*?\\)"]
+const constraints = [ `NOT NULL`, `DEFAULT\\s[\\w\\(\\)'"]+`, `UNIQUE`, `PRIMARY KEY\\s*\\(.*?\\)`]
 const regex_types = types.join('|');
 const regex_constraints2 = constraints.join('|');
 const primary_key_newline = /^\s*PRIMARY\sKEY\s*\((?<columns>.+?)\)\s*$/gmi;
@@ -15,6 +15,7 @@ for (let i = 0; i < 10; i++) {
     regex_constraints += `(?<constrain${i}>${regex_constraints2})?\\s?`
 }
 let regex_columns = new RegExp(`^\\s*"(?<name>\\w+)"\\s((?<type>(${regex_types})+(\\(.+\\))?)(\\(\\d\\))?)\\s*${regex_constraints},\\W*?$`, 'gmi');
+console.log('regex_columns', regex_columns);
 
 // others
 const others_names = /CREATE\s+(?:OR\s+REPLACE\s*)|(FUNCTION|PROCEDURE|VIEW)\s+"?(?<schema>[\w]*)?"?\.?"?(?<name>(fn|view|procedure)_[\w]+)"?\s?(\(|AS)/gmi;
@@ -28,5 +29,4 @@ module.exports = {
     index_line, 
     others_names,
     others_dependencies
-
 };
